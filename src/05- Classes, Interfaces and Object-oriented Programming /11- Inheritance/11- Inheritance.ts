5d168f97de894c67788865866173d38b7ef8887d
Reacher <macryze@icloud.com>
2025-03-05 13:40:09 +0800

    Comment out Person and Student class implementation
    The code for the Person and Student classes, along with the usage example, has been commented out. This change might be preparation for refactoring, debugging, or temporarily disabling the functionality without deleting the code.

--- src/05- Classes, Interfaces and Object-oriented Programming /11- Inheritance/11- Inheritance.ts
+++ src/05- Classes, Interfaces and Object-oriented Programming /11- Inheritance/11- Inheritance.ts
@@ -1,41 +1,41 @@
-// ===========================================================
-//                      Person Class
-// ===========================================================
-class Person {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-  ) {}
-
-  get fullName() {
-    return this.firstName + " " + this.lastName;
-  }
-
-  walk() {
-    console.log("Walking");
-  }
-}
-
-// ===========================================================
-//                      Student Class (Inheritance)
-// ===========================================================
-class Student extends Person {
-  // Using access modifier in constructor auto-initializes properties (e.g., this.studentId = studentId)
-  constructor(
-    public studentId: number,
-    firstName: string,
-    lastName: string,
-  ) {
-    super(firstName, lastName);
-  }
-
-  takeTest() {
-    console.log("Taking a test");
-  }
-}
-
-// ===========================================================
-//                      Usage Example
-// ===========================================================
-let student = new Student(1, "Reacher", "email@email.com");
-console.log(student);
+// // ===========================================================
+// //                      Person Class
+// // ===========================================================
+// class Person {
+//   constructor(
+//     public firstName: string,
+//     public lastName: string,
+//   ) {}
+//
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+//
+//   walk() {
+//     console.log("Walking");
+//   }
+// }
+//
+// // ===========================================================
+// //                      Student Class (Inheritance)
+// // ===========================================================
+// class Student extends Person {
+//   // Using access modifier in constructor auto-initializes properties (e.g., this.studentId = studentId)
+//   constructor(
+//     public studentId: number,
+//     firstName: string,
+//     lastName: string,
+//   ) {
+//     super(firstName, lastName);
+//   }
+//
+//   takeTest() {
+//     console.log("Taking a test");
+//   }
+// }
+//
+// // ===========================================================
+// //                      Usage Example
+// // ===========================================================
+// let student = new Student(1, "Reacher", "email@email.com");
+// console.log(student);
