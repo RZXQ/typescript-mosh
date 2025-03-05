9df4b95845c4adda803a9376e6e4220a1a2a7bf4
Reacher <macryze@icloud.com>
2025-03-05 18:51:40 +0800

    "Comment out Person, Student, and Teacher classes"
    The Person, Student, and Teacher class implementations were fully commented out. This change might be intended for temporary deactivation or refactoring without deleting the code.

--- src/05- Classes, Interfaces and Object-oriented Programming /14- Private vs Protected Members/14- Private vs Protected Members.ts
+++ src/05- Classes, Interfaces and Object-oriented Programming /14- Private vs Protected Members/14- Private vs Protected Members.ts
@@ -1,45 +1,45 @@
-// ===========================================================
-//                      Person Class
-// ===========================================================
-class Person {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-  ) {}
-
-  get fullName() {
-    return this.firstName + " " + this.lastName;
-  }
-
-  // Note: Protected method; prefer public/private to reduce coupling
-  protected walk() {
-    console.log("Walking");
-  }
-}
-
-// ===========================================================
-//                      Student Class
-// ===========================================================
-class Student extends Person {
-  constructor(
-    public studentId: number,
-    firstName: string,
-    lastName: string,
-  ) {
-    super(firstName, lastName);
-  }
-
-  takeTest() {
-    this.walk(); // Accessing protected method from parent
-    console.log("Taking a test");
-  }
-}
-
-// ===========================================================
-//                      Teacher Class
-// ===========================================================
-class Teacher extends Person {
-  override get fullName() {
-    return `Professor: ${super.fullName}`;
-  }
-}
+// // ===========================================================
+// //                      Person Class
+// // ===========================================================
+// class Person {
+//   constructor(
+//     public firstName: string,
+//     public lastName: string,
+//   ) {}
+//
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+//
+//   // Note: Protected method; prefer public/private to reduce coupling
+//   protected walk() {
+//     console.log("Walking");
+//   }
+// }
+//
+// // ===========================================================
+// //                      Student Class
+// // ===========================================================
+// class Student extends Person {
+//   constructor(
+//     public studentId: number,
+//     firstName: string,
+//     lastName: string,
+//   ) {
+//     super(firstName, lastName);
+//   }
+//
+//   takeTest() {
+//     this.walk(); // Accessing protected method from parent
+//     console.log("Taking a test");
+//   }
+// }
+//
+// // ===========================================================
+// //                      Teacher Class
+// // ===========================================================
+// class Teacher extends Person {
+//   override get fullName() {
+//     return `Professor: ${super.fullName}`;
+//   }
+// }
