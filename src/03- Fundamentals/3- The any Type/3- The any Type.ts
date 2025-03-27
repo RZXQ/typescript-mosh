0038ac3edfe65b98ebb3ee6a971c30902188ae72
Reacher <macryze@icloud.com>
2025-03-27 10:37:09 +0800

    Comment out `any` type usage examples in code.
    The examples demonstrating the `any` type have been commented out to prevent unintentional usage. This ensures the code aligns better with TypeScript's best practices for type safety without deleting the illustrative examples.

--- src/03- Fundamentals/3- The any Type/3- The any Type.ts
+++ src/03- Fundamentals/3- The any Type/3- The any Type.ts
@@ -1,14 +1,14 @@
-// ===========================================================
-//                        Avoid `any` Type
-// Note: Avoid using the `any` type as it disables type checking,
-// losing TypeScript's type safety.
-// ===========================================================
-
-let level; // Implicit `any` should be avoided.
-level = 1;
-level = "a";
-
-// Using `any` in function parameters removes type safety.
-function render(document: any) {
-    console.log(document);
-}
+// // ===========================================================
+// //                        Avoid `any` Type
+// // Note: Avoid using the `any` type as it disables type checking,
+// // losing TypeScript's type safety.
+// // ===========================================================
+//
+// let level; // Implicit `any` should be avoided.
+// level = 1;
+// level = "a";
+//
+// // Using `any` in function parameters removes type safety.
+// function render(document: any) {
+//     console.log(document);
+// }
