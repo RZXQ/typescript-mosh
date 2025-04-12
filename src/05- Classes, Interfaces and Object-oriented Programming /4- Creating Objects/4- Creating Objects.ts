90499daf665fad1b1dc7e13e73619a4fe62f4e39
Reacher <macryze@icloud.com>
2025-04-12 20:47:46 +0800

    Refactor: Uncomment and enable Account class implementation
    Restored the previously commented-out Account class and its usage example. This ensures the class definition and its functionality are active, including logging and type-checking capabilities. Added an export statement to allow external use of the class.

--- src/05- Classes, Interfaces and Object-oriented Programming /4- Creating Objects/4- Creating Objects.ts
+++ src/05- Classes, Interfaces and Object-oriented Programming /4- Creating Objects/4- Creating Objects.ts
@@ -1,31 +1,33 @@
-// // ===========================================================
-// //                Account Class Definition
-// // ===========================================================
-//
-// class Account {
-//   id: number;
-//   owner: string;
-//   amount: number;
-//
-//   constructor(id: number, owner: string, amount: number) {
-//     this.id = id;
-//     this.owner = owner;
-//     this.amount = amount;
-//   }
-//
-//   deposit(depositAmount: number) {
-//     if (depositAmount <= 0) throw new Error("Invalid amount");
-//     this.amount += depositAmount;
-//   }
-// }
-//
-// // ===========================================================
-// //                      Account Usage
-// // ===========================================================
-//
-// const account = new Account(1, "Reacher", 0);
-// account.deposit(100);
-//
-// console.log(account); // Display the account's current state
-// console.log(typeof account); // Check the type of the account instance
-// console.log(account instanceof Account); // Verify if bankAccount is an instance of Account
+// ===========================================================
+//                Account Class Definition
+// ===========================================================
+
+class Account {
+  id: number;
+  owner: string;
+  amount: number;
+
+  constructor(id: number, owner: string, amount: number) {
+    this.id = id;
+    this.owner = owner;
+    this.amount = amount;
+  }
+
+  deposit(depositAmount: number) {
+    if (depositAmount <= 0) throw new Error("Invalid amount");
+    this.amount += depositAmount;
+  }
+}
+
+// ===========================================================
+//                      Account Usage
+// ===========================================================
+
+const account = new Account(1, "Reacher", 0);
+account.deposit(100);
+
+console.log(account); // Display the account's current state
+console.log(typeof account); // Check the type of the account instance
+console.log(account instanceof Account); // Verify if bankAccount is an instance of Account
+
+export = {};
