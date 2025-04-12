5bfa94dfd09e21d859caddcfd794da8b53f96d47
Reacher <macryze@icloud.com>
2025-04-12 15:30:50 +0800

    Refine comments on type assertions for clarity
    Add clarification that type assertions do not convert types but merely assert them, avoiding potential misconceptions about their functionality. This ensures better understanding and accurate usage of type assertions.

--- src/04- Advanced Types/9- Type Assertions/9- Type Assertions.ts
+++ src/04- Advanced Types/9- Type Assertions/9- Type Assertions.ts
@@ -7,6 +7,7 @@
 // - Since we know it's an `<input>`, we use Type Assertion (`as HTMLInputElement`) to specify its type.
 // - This is similar to downcasting (polymorphism) in object-oriented programming.
 // - Caution: If the element is not an `<input>`, using `as HTMLInputElement` will cause a runtime error.
+// There is no type converting when using as XXX(type assertion), not narrowing it to another type
 
 let phone = document.getElementById("phone") as HTMLInputElement;
 
