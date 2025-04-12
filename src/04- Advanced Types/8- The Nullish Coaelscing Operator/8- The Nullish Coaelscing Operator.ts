00d23dc1a7ceb1f6781545d6150f8a3e0e29d538
Reacher <macryze@icloud.com>
2025-04-12 15:21:54 +0800

    Refactor and clarify nullish coalescing operator example
    Updated examples to better explain falsy values and comparison approaches. Improved comments for clarity and reorganized code to enhance readability and understanding.

--- src/04- Advanced Types/8- The Nullish Coaelscing Operator/8- The Nullish Coaelscing Operator.ts
+++ src/04- Advanced Types/8- The Nullish Coaelscing Operator/8- The Nullish Coaelscing Operator.ts
@@ -1,24 +1,32 @@
 // ===========================================================
 //                  1. Falsy Values in JavaScript
 // ===========================================================
-// - Falsy values: null, undefined, '', 0, false
-
-let speed: number | null = null;
-
+// Falsy values: 0 -0 0n NaN '' false null undefined
+//
 // ===========================================================
 //       2. Traditional Way to Check Falsy Values
 // ===========================================================
+// Approach 1: using the || to check :
+// let ride = {
+//    speed: speed || 30; // but this would ignore the falsy value like 0
+// }
+//
+// Approach 2:
 // - Checks null and undefined explicitly
-// speed: speed !== null ? speed : 30;
-
+// speed !== null && speed !== undefined ? speed : 30;
+//
 // ===========================================================
 //       3. TypeScript Way: Nullish Coalescing Operator (??)
 // ===========================================================
 // - If `speed` is not null or undefined, use that value
 // - Otherwise, use the fallback value (30)
 
+let speed: number | null = null;
+
 let ride = {
   speed: speed ?? 30,
 };
 
 console.log(ride); // { speed: 30 }
+
+export = {};
