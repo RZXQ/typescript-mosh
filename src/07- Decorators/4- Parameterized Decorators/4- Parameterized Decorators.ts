5feb5fe2ce75512f5776aaecad068922b66e1402
Reacher <macryze@icloud.com>
2025-06-15 11:48:10 +0800

    Add parameterized decorator example with explanation
    Introduced a new TypeScript file illustrating parameterized decorators. Explained the use of decorator factories for creating reusable, configurable decorators and provided a clear example with comments.

---
+++ src/07- Decorators/4- Parameterized Decorators/4- Parameterized Decorators.ts
@@ -0,0 +1,14 @@
+function Component(value: number) {
+  return (constructor: Function) => {
+    constructor.prototype.options = value;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () =>
+      console.log("Inserting the component in the DOM");
+  };
+}
+
+// Decorator Factory: A function that creates/manufactures decorator functions.
+// Takes configuration parameters and returns a customized decorator.
+// Follows the Factory Pattern - reusable and configurable.
+@Component(10)
+class ProfileComponent {}
