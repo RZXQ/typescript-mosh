bd426c1573b7d6d3ebe6b806edf502d290c24cbd
Reacher <macryze@icloud.com>
2025-06-15 10:30:15 +0800

    Add generics exercises with examples and explanations
    Introduced new exercises demonstrating key concepts in generics: generic functions, type constraints, generic classes, and the `keyof` operator. Included clear comments to enhance understanding.

---
+++ src/06- Generics/11- Exercises/11- Exercises.ts
@@ -0,0 +1,35 @@
+// 1. Convert the function below to a generic function:
+// function echo(arg) {
+//     return arg;
+// }
+
+function echo<T>(arg: T): T {
+  return arg;
+}
+
+// 2. When compiling the following piece of code, we get an error saying
+// 'Property name does not exist on type T'. How can we solve this problem?
+// function printName<T>(obj: T) {
+//     console.log(obj.name);
+// }
+
+function printName<T extends { name: string }>(obj: T) {
+  console.log(obj.name);
+}
+
+// 3. An Entity should have a unique identifier. The type of identifier,
+// however, is dependent on the use case. In some cases, the ID might be a
+// number, in other cases, it might be a string, GUID, etc. Represent the
+// entity using a generic class.
+
+class Entity<T> {
+  constructor(public id: T) {}
+}
+
+// 4. Given the following interface, what does keyof User return?
+// interface User {
+//     userId: number;
+//     username: string;
+// }
+
+// It returns a union of the properties of User: 'userId' | 'username'
