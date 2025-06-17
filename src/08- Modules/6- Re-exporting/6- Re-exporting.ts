183a2a78d2e28122ab3a6893172ebba0c8e0d848
Reacher <macryze@icloud.com>
2025-06-17 15:07:46 +0800

    Remove re-exporting example from TypeScript module explanation files

--- src/08- Modules/6- Re-exporting/6- Re-exporting.ts
+++
@@ -1,5 +0,0 @@
-// Traditional way: Import first, then export separately
-
-// import { Circle } from "./circle";
-// import { Square } from "./square";
-// export { Circle, Square };
--- src/08- Modules/6- Re-exporting/shapes/index.ts
+++ src/08- Modules/6- Re-exporting/shapes/index.ts
@@ -1,4 +1,8 @@
-// Re-exporting: Import and export in a single statement
+// Traditional way: Import first, then export separately
+// import { Circle } from "./circle";
+// import { Square } from "./square";
+// export { Circle, Square };
 
+// Re-exporting: Import and export in a single statement
 // export { Circle } from "./circle";
 // export { Square } from "./square";
